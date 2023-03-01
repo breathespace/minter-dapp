e5ab1b2607b12cf896fed33ae67a4c96c890f0d5
breathespace <michael_wallstein@yahoo.de>
2023-03-01 14:44:59 +0100

    main

--- backend/src/config.js
+++ backend/src/config.js
@@ -65,7 +65,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "YOUR CONTRACT ADDRESS"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x2a481a3405fc4cc9440c4d19f7de82102E94cF80"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = false; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
